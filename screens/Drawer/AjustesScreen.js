dafa10e0ec356eb27d2c5feb7d223c93ea8b3dba
JSalMuo <jsalmuo@etsinf.upv.es>
2023-11-22 08:52:56 +0100

    Revert "Merge branch 'branch-Jesús' into main"
    This reverts commit cfa1a26378e04f05392fcd002aa7ae0c498ba096, reversing
    changes made to f79303ce45b89a0b3e456b0b3397e67bffe4fe7d.

--- screens/Drawer/AjustesScreen.js
+++ screens/Drawer/AjustesScreen.js
@@ -1,90 +1,9 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import Slider from '@react-native-community/slider';
+import { View, Text } from 'react-native';
 
-const AjustesScreen = () => {
-  const [generalVolume, setGeneralVolume] = useState(0);
-  const [musicVolume, setMusicVolume] = useState(0);
-  const [sfxVolume, setSfxVolume] = useState(0);
-
-  const onSlider1ValueChange = (value) => {
-    setGeneralVolume(value);
-  };
-  const onSlider2ValueChange = (value) => {
-    setMusicVolume(value);
-  };
-  const onSlider3ValueChange = (value) => {
-    setSfxVolume(value);
-  };
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.volumeControl}>
-        <Text style={styles.volumeLabel}>Volumen general: {Math.round(generalVolume * 100)}%</Text>
-        <Slider
-          style={styles.slider}
-          minimumValue={0}
-          maximumValue={1}
-          value={generalVolume}
-          onValueChange={onSlider1ValueChange}
-          thumbTintColor="#3498db"
-          minimumTrackTintColor="#3498db"
-          maximumTrackTintColor="#bdc3c7"
-        />
-      </View>
-      <View style={styles.volumeControl}>
-        <Text style={styles.volumeLabel}>Música: {Math.round(musicVolume * 100)}%</Text>
-        <Slider
-          style={styles.slider}
-          minimumValue={0}
-          maximumValue={1}
-          value={musicVolume}
-          onValueChange={onSlider2ValueChange}
-          thumbTintColor="#3498db"
-          minimumTrackTintColor="#3498db"
-          maximumTrackTintColor="#bdc3c7"
-        />
-      </View>
-      <View style={styles.volumeControl}>
-        <Text style={styles.volumeLabel}>Efectos de sonido: {Math.round(sfxVolume * 100)}%</Text>
-        <Slider
-          style={styles.slider}
-          minimumValue={0}
-          maximumValue={1}
-          value={sfxVolume}
-          onValueChange={onSlider3ValueChange}
-          thumbTintColor="#3498db"
-          minimumTrackTintColor="#3498db"
-          maximumTrackTintColor="#bdc3c7"
-        />
-      </View>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  volumeControl: {
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-  volumeLabel: {
-    fontSize: 16,
-    marginBottom: 10,
-  },
-  slider: {
-    width: 200,
-  },
-});
-
-export default AjustesScreen;
+export default function AjustesScreen() {
+    return (
+        <View>
+            <Text>AjustesScreen</Text>
+        </View>
+    );
+}
