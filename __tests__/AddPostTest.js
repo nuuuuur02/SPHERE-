86b5dda93ee4fbfe00c5fa40c079947d09fa0a28
JBonifas8 <jbonifas08@gmail.com>
2023-12-02 10:38:12 +0100

    retoque

--- __tests__/AddPostTest.js
+++ __tests__/AddPostTest.js
@@ -6,14 +6,14 @@ import AddPostScreen from './AddPostScreen';
 jest.mock('@react-native-firebase/auth', () => ({
   currentUser: {
     displayName: 'TestUser',
-    photoURL: 'https://example.com/avatar.jpg',
+    photoURL: 'https://https://media.licdn.com/dms/image/D4E03AQFfxLOdeC7DuQ/profile-displayphoto-shrink_400_400/0/1682706569822?e=1703116800&v=beta&t=852VlgPDbMXRP_Lv-xF2rjKU-3GWIepCD1DbHUYa6_g.com/avatar.jpg',
   },
 }));
 
 jest.mock('@react-native-firebase/storage', () => ({
   ref: jest.fn(),
   uploadBytesResumable: jest.fn(),
-  getDownloadURL: jest.fn().mockResolvedValue('https://example.com/post.jpg'),
+  getDownloadURL: jest.fn().mockResolvedValue('https://firebasestorage.googleapis.com/v0/b/niideapepe-45402.appspot.com/o/Images%2F1701364928237?alt=media&token=7327ae8f-c52b-4c46-b416-ad94df39c6bd'),
 }));
 
 jest.mock('@react-native-firebase/firestore', () => ({
@@ -28,11 +28,11 @@ jest.mock('@react-native-firebase/firestore', () => ({
 jest.mock('expo-image-picker', () => ({
   launchImageLibraryAsync: jest.fn().mockResolvedValue({
     cancelled: false,
-    assets: [{ uri: 'file://path/to/image.jpg' }],
+    assets: [{ uri: 'https://media.licdn.com/dms/image/D4E03AQFfxLOdeC7DuQ/profile-displayphoto-shrink_400_400/0/1682706569822?e=1703116800&v=beta&t=852VlgPDbMXRP_Lv-xF2rjKU-3GWIepCD1DbHUYa6_g' }],
   }),
   launchCameraAsync: jest.fn().mockResolvedValue({
     cancelled: false,
-    assets: [{ uri: 'file://path/to/image.jpg' }],
+    assets: [{ uri: 'https://media.licdn.com/dms/image/D4E03AQFfxLOdeC7DuQ/profile-displayphoto-shrink_400_400/0/1682706569822?e=1703116800&v=beta&t=852VlgPDbMXRP_Lv-xF2rjKU-3GWIepCD1DbHUYa6_g' }],
   }),
 }));
 
@@ -58,9 +58,9 @@ describe('AddPostScreen', () => {
 
     expect(addDoc).toHaveBeenCalledWith(expect.anything(), {
       post: 'This is a test post',
-      postImg: 'https://example.com/post.jpg', 
+      postImg: 'https://firebasestorage.googleapis.com/v0/b/niideapepe-45402.appspot.com/o/Images%2F1701364928237?alt=media&token=7327ae8f-c52b-4c46-b416-ad94df39c6bd', 
       userName: 'TestUser',
-      userImg: 'https://example.com/avatar.jpg', 
+      userImg: 'https://media.licdn.com/dms/image/D4E03AQFfxLOdeC7DuQ/profile-displayphoto-shrink_400_400/0/1682706569822?e=1703116800&v=beta&t=852VlgPDbMXRP_Lv-xF2rjKU-3GWIepCD1DbHUYa6_g', 
     });
   });
 });
