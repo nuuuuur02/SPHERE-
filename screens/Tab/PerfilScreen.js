52695214ed0d76b100dede024061b1e1b3baaabd
JBonifas8 <jbonifas08@gmail.com>
2023-12-06 20:34:50 +0100

    añadir diaros check

--- screens/Tab/PerfilScreen.js
+++ screens/Tab/PerfilScreen.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, TouchableOpacity,Image } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
 import { auth, db } from '../../components/ConfigFirebase';
 import { doc, getDoc } from 'firebase/firestore';
 import { format } from 'date-fns';
+import { useFocusEffect } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
 import {
   CardDiary,
   CardDiaryCom,
@@ -11,92 +13,104 @@ import {
   NoteButton,
   UserImgDiary,
   UserNameDiary,
-  userFeelingImg,
 } from '../../styles/FeedStyles';
-import { FontAwesome } from '@expo/vector-icons';
-import { useFocusEffect } from '@react-navigation/native';
+
 
 export default function PerfilScreen({ navigation }) {
-
-  
   const [userData, setUserData] = useState(null);
   const [isProfessionalEnabled, setIsProfessional] = useState(false);
   const [isFamiliarEnabled, setIsFamiliar] = useState(false);
   const [diaryCards, setDiaryCards] = useState([]);
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(async (user) => {
-      if (user) {
-        setUserData({
-          uid: user.uid,
-          photo: user.photoURL,
-          email: user.email,
-          nombre: user.displayName,
-        });
+  const loadUserData = async () => {
+    const user = auth.currentUser;
 
-        const userDocRef = doc(db, 'user', user.uid);
-        const userDoc = await getDoc(userDocRef);
+    if (user) {
+      setUserData({
+        uid: user.uid,
+        photo: user.photoURL,
+        email: user.email,
+        nombre: user.displayName,
+      });
 
-        if (userDoc.exists()) {
-          const userData = userDoc.data();
-          setIsProfessional(userData.isProfesional);
-          setIsFamiliar(userData.isFamiliar);
+      const userDocRef = doc(db, 'user', user.uid);
+      const userDoc = await getDoc(userDocRef);
 
-          // Obtén las tarjetas del diario y guárdalas en el estado
-          setDiaryCards(userData.diaryCards || []);
-        }
-      } else {
-        setUserData(null);
-        setIsProfessional(false);
-        setIsFamiliar(false);
-        setDiaryCards([]);
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+        setIsProfessional(userData.isProfesional);
+        setIsFamiliar(userData.isFamiliar);
+        setDiaryCards(userData.diaryCards || []);
       }
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      loadUserData();
     });
 
-    return () => unsubscribe();
-  }, []);
+    return () => {
+      unsubscribe();
+    };
+  }, [navigation]);
 
-  const renderItem = ({ item }) => (
-    <CardDiaryCom style={{ flex: 1 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingLeft: 25, paddingRight: 25, paddingTop: 15 }}>
-        <Text>{format(item.diaryTime.toDate(), 'dd/MM/yyyy HH:mm')}</Text>
-        <TouchableOpacity onPress={() => console.log('Editar')}>
-          <FontAwesome name="pencil" size={20} color="black" />
-        </TouchableOpacity>
-      </View>
-      <View>
-        <DiaryText>{item.diaryText}</DiaryText>
-      </View>
+  const renderItem = ({ item }) => {
+    let feelingImageSource;
 
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-      <DiaryText>Hoy me siento: </DiaryText>
-      <Image
-        source={require('../../assets/iconos/caritacontenta.png')}
-        
-      />
-      </View>
+    switch (item.feeling) {
+      case 'caritacontenta':
+        feelingImageSource = require('../../assets/iconos/caritacontenta.png');
+        break;
+      case 'caritatranquila':
+        feelingImageSource = require('../../assets/iconos/caritatranquila.png');
+        break;
+      case 'caritaenfadada':
+        feelingImageSource = require('../../assets/iconos/caritaenfadada.png');
+        break;
+      case 'caritatriste':
+        feelingImageSource = require('../../assets/iconos/caritatriste.png');
+        break;
+      default:
+        feelingImageSource = null; // Manejar el caso por defecto o establecerlo en una imagen predeterminada
+    }
 
-    </CardDiaryCom>
-  );
+    return (
+      <CardDiaryCom style={{ flex: 1 }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingLeft: 25, paddingRight: 25, paddingTop: 15 }}>
+          <Text>{format(item.diaryTime.toDate(), 'dd/MM/yyyy HH:mm')}</Text>
+          <TouchableOpacity onPress={() => console.log('Editar')}>
+            <FontAwesome name="pencil" size={20} color="black" />
+          </TouchableOpacity>
+        </View>
+        <View>
+          <DiaryText>{item.diaryText}</DiaryText>
+        </View>
+
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <DiaryText>Hoy me siento: </DiaryText>
+          {feelingImageSource && (
+            <Image
+              source={feelingImageSource}
+              
+            />
+          )}
+        </View>
+      </CardDiaryCom>
+    );
+  };
 
   return (
     <View style={{ flex: 1, backgroundColor: '#EBEBEB' }}>
-
       <UserImgDiary source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYqLHetw4u8ODXWqrunrcK_YeLbyQayrxfYVZrvyYiMw&s' }} />
       <UserNameDiary>{auth.currentUser.displayName}</UserNameDiary>
-
-
-
-
       <CardDiary style={{ marginTop: 35, backgroundColor: '#fff' }}>
         <AddDiaryBar>
           <Text style={{ fontSize: 20, fontWeight: 'bold', alignSelf: 'center' }}>Diario</Text>
-          <NoteButton onPress={() => navigation.navigate('AddDiaryScreen')}>
+          <NoteButton onPress={() => navigation.navigate('AddDiaryScreen', { loadUserData })}>
             <Text>AÑADIR NOTA</Text>
           </NoteButton>
         </AddDiaryBar>
-
-        {/* Renderiza las tarjetas del diario */}
         <FlatList
           data={diaryCards}
           renderItem={renderItem}
