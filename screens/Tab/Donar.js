1130e4f390591f403dcca4f92f07d51e01836fc3
adria frasquet <aframas@upv.edu.es>
2023-12-16 21:10:12 +0100

    CasiDonar

--- screens/Tab/Donar.js
+++ screens/Tab/Donar.js
@@ -1,14 +1,46 @@
 import * as React from 'react';
 import { View, Text, Button, StyleSheet, Image, ScrollView, FlatList, TextInput } from 'react-native';
+import {Divider, useTheme } from '@rneui/themed';
 
 export default function Donar({ navigation, route }) {
-  
+  const [tarjeta, setTarjeta] = React.useState('Numero de tarjeta');
   const {item} = route.params
   return (
     <View style={Styles.fondo}>
         <Text style={Styles.titulo}>Introduce tus datos</Text>
         <Text style={Styles.textos}>Número de tarjeta</Text>
-        <TextInput></TextInput>
+        <TextInput
+        style={Styles.input}
+        onChangeText={setTarjeta}
+        value={tarjeta}
+      />
+      <View style={Styles.vertical}>
+        <View style={Styles.horizontal}>
+            <Text style={Styles.textos}>Caduca</Text>
+
+            <Divider orientation="horizontal" />
+
+            <TextInput
+                style={Styles.input}
+                onChangeText={setTarjeta}
+                value={tarjeta}
+            />
+        </View>
+
+        <Divider orientation="vertical" />
+
+        <View style={Styles.horizontal}>
+            <Text style={Styles.textos}>CVV</Text>
+
+            <Divider orientation="horizontal" />
+
+            <TextInput
+                style={Styles.input}
+                onChangeText={setTarjeta}
+                value={tarjeta}
+            />
+        </View>
+      </View>
     </View>
   );
 };
@@ -22,11 +54,30 @@ const Styles = StyleSheet.create({
         fontWeight: 'bold',
         marginBottom: 60,
     },
+    input:{
+        height: 40,
+        marginHorizontal: 20,
+        borderWidth: 0,
+        padding: 10,
+        paddingHorizontal: 10,
+        borderRadius:20,
+        backgroundColor: '#EBEBEB'
+    },
     foto:{
         marginLeft: 30,
         marginRight:30,
         height: 300,
         width: 300,
+    },
+    vertical: {
+        marginVertical: 10,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+    },
+    textosVertical:{
+        textAlign: 'center',
+        textAlignVertical: 'center'
     },
     fondo:{
         backgroundColor: 'white',
